ec3be95d3f93f96c599cd351295414364abd6710
Rafael Teixeira <rafaelteixeira@digix.com.br>
2018-11-27 13:14:14 -0300

    Configuracao de ambiente

--- Infra/Fila/Configuracao/Ambiente.cs
+++ Infra/Fila/Configuracao/Ambiente.cs
@@ -3,13 +3,14 @@ namespace Ocr.Infra.Fila.Configuracao
   public static class Ambiente
   {
     private static string topicoDoArquivoNaoProcessado;
+    private static string topicoDoArquivoProcessado;
     private static string hostDaFila;
     private static string topicoDoArquivoComErro;
 
     public static string TopicoDoArquivoNaoProcessado
     {
       get => string.IsNullOrEmpty(topicoDoArquivoNaoProcessado)
-        ? "ArquivosNaoProcessados_Dev"
+        ? "ArquivosNaoProcessados"
         : topicoDoArquivoNaoProcessado;
       set
       {
@@ -19,19 +20,19 @@ namespace Ocr.Infra.Fila.Configuracao
 
     public static string TopicoDoArquivoProcessado
     {
-      get => string.IsNullOrEmpty(topicoDoArquivoNaoProcessado)
-        ? "ArquivosProcessados_Dev"
-        : topicoDoArquivoNaoProcessado;
+      get => string.IsNullOrEmpty(topicoDoArquivoProcessado)
+        ? "ArquivosProcessados"
+        : topicoDoArquivoProcessado;
       set
       {
-        topicoDoArquivoNaoProcessado = value;
+        topicoDoArquivoProcessado = value;
       }
     }
 
     public static string TopicoDoArquivoComErro
     {
       get => string.IsNullOrEmpty(topicoDoArquivoComErro)
-        ? "ArquivosComErro_Dev"
+        ? "ArquivosComErro"
         : topicoDoArquivoComErro;
       set
       {
